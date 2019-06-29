b1494a6c4aed62750de2b4dbdbcbcafca04135f7
xiangguo <guo.xiang@56qq.com>
2019-06-29 12:55:30 +0800

    pc首页

--- i100.js
+++ i100.js
@@ -1,66 +1,66 @@
 iweb.controller('i100', function($scope,$routeParams) {
-    // setTimeout(()=>{
-    //     $('body,html').scrollTop(0);
-    // },0)
+    setTimeout(()=>{
+        $('body,html').scrollTop(0);
+    },0)
+
+    // $(window).resize(function(){
+    //     if($(window).width()>993){
+    //         if(typeof $.fn.fullpage.destroy==='function'){
+    //             return
+    //         }
+    //         $('#fullpage').fullpage({
+    //             scrollBar:true,
+    //             onLeave: function(index, nextIndex, direction){
+    //                 if(nextIndex!==1||nextIndex!==7){
+    //                     $('.headerNav').css('top','-100px')
+    //                 }else{
+    //                     $('.headerNav').css('top','0')
+    //                 }
+    //             },
+    //         });
+    //     }else{
+    //         if($.fn.fullpage.destroy){
+    //             $.fn.fullpage.destroy('all');
+    //         }
     //
-    // // $(window).resize(function(){
-    // //     if($(window).width()>993){
-    // //         if(typeof $.fn.fullpage.destroy==='function'){
-    // //             return
-    // //         }
-    // //         $('#fullpage').fullpage({
-    // //             scrollBar:true,
-    // //             onLeave: function(index, nextIndex, direction){
-    // //                 if(nextIndex!==1||nextIndex!==7){
-    // //                     $('.headerNav').css('top','-100px')
-    // //                 }else{
-    // //                     $('.headerNav').css('top','0')
-    // //                 }
-    // //             },
-    // //         });
-    // //     }else{
-    // //         if($.fn.fullpage.destroy){
-    // //             $.fn.fullpage.destroy('all');
-    // //         }
-    // //
-    // //     }
-    // // })
-    //
-    // if($(window).width()>993) {
-    //     $('#fullpage').fullpage({
-    //         scrollBar: true,
-    //         onLeave: function (index, nextIndex, direction) {
-    //             if (nextIndex !== 1&&nextIndex!==7) {
-    //                 $('.headerNav').css('top', '-100px')
-    //             } else {
-    //                 $('.headerNav').css('top', '0')
-    //             }
-    //             if(nextIndex !==7){
-    //                 $('.footer').css('bottom','-200px')
-    //             }else{
-    //                 $('.footer').css('bottom',0)
-    //             }
-    //         },
-    //         lazyLoading: true,
-    //       afterRender: function(){
-    //         var pluginContainer = $(this);
-    //         alert("The resulting DOM structure is ready");
-    //       }
-    //         // afterLoad: function(anchorLink, index){
-    //         //     alert(index)
-    //         // },
-    //     });
-    // }
-    //
-    // var swiper = new Swiper('.swiper-container', {
-    //     slidesPerView: 8,
-    //     spaceBetween: 5,
-    //     freeMode:true,
-    //     pagination: {
-    //         el: '.swiper-pagination',
-    //         clickable: true,
-    //     },
-    // });
+    //     }
+    // })
+
+    if($(window).width()>993) {
+        $('#fullpage').fullpage({
+            scrollBar: true,
+            onLeave: function (index, nextIndex, direction) {
+                if (nextIndex !== 1&&nextIndex!==7) {
+                    $('.headerNav').css('top', '-100px')
+                } else {
+                    $('.headerNav').css('top', '0')
+                }
+                if(nextIndex !==7){
+                    $('.footer').css('bottom','-200px')
+                }else{
+                    $('.footer').css('bottom',0)
+                }
+            },
+            lazyLoading: true,
+          afterRender: function(){
+            var pluginContainer = $(this);
+            alert("The resulting DOM structure is ready");
+          }
+            // afterLoad: function(anchorLink, index){
+            //     alert(index)
+            // },
+        });
+    }
+
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 8,
+        spaceBetween: 5,
+        freeMode:true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
 
 
 })
