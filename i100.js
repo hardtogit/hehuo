3ff474eda2e5d7c5e38f5538338a7c0eee7ff977
xiangguo <guo.xiang@56qq.com>
2019-06-29 13:08:09 +0800

    pc首页

--- i100.js
+++ i100.js
@@ -27,29 +27,32 @@ iweb.controller('i100', function($scope,$routeParams) {
     // })
 
     if($(window).width()>993) {
+      $(function () {
         $('#fullpage').fullpage({
-            scrollBar: true,
-            onLeave: function (index, nextIndex, direction) {
-                if (nextIndex !== 1&&nextIndex!==7) {
-                    $('.headerNav').css('top', '-100px')
-                } else {
-                    $('.headerNav').css('top', '0')
-                }
-                if(nextIndex !==7){
-                    $('.footer').css('bottom','-200px')
-                }else{
-                    $('.footer').css('bottom',0)
-                }
-            },
-            lazyLoading: true,
+          scrollBar: true,
+          onLeave: function (index, nextIndex, direction) {
+            if (nextIndex !== 1&&nextIndex!==7) {
+              $('.headerNav').css('top', '-100px')
+            } else {
+              $('.headerNav').css('top', '0')
+            }
+            if(nextIndex !==7){
+              $('.footer').css('bottom','-200px')
+            }else{
+              $('.footer').css('bottom',0)
+            }
+          },
+          lazyLoading: true,
           afterRender: function(){
             var pluginContainer = $(this);
             alert("The resulting DOM structure is ready");
           }
-            // afterLoad: function(anchorLink, index){
-            //     alert(index)
-            // },
+          // afterLoad: function(anchorLink, index){
+          //     alert(index)
+          // },
         });
+      })
+
     }
 
     var swiper = new Swiper('.swiper-container', {
